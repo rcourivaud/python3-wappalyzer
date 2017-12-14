c217d5f8b32faf7984af28dd8714f9a89f029fbd
Raphaël Courivaud <r.courivaud@gmail.com>
2017-12-14 15:44:43 +0100

    update setup.py and Manifest

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from .Wappalyzer import WebPage, Wappalyzer
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='wappalyzer',
+    version='',
+    packages=['wappalyzer'],
+    url='',
+    license='',
+    author='Raphaël Courivaud',
+    author_email='r.courivaud@gmail.com',
+    package_data={'wappalyzer': ['data/*']},
+    description=''
+)
